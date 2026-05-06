26ca3508ee476e4ec4144c1c07442d71b736c753
Aurel Ionita <ionitaaurel32@gmail.com>
2026-05-06 22:54:42 +0300

    refactor(pages): remove unused Contact component import

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,7 +7,7 @@ import Events from '@/components/Events';
 import Map from '@/components/Map';
 import DeviceShowcase from '@/components/DeviceShowcase';
 import FAQ from '@/components/FAQ';
-import Contact from '@/components/Contact';
+
 import Footer from '@/components/Footer';
 
 const Index = () => {
